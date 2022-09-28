66bcd563a0bc0389dde17ac935d037256953b514
MD Shahinur Rahman Nayon <mdshahinurrahmannayon@gmail.com>
2022-09-29 05:22:07 +0600

    question and answer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,21 @@ const Footer = () => {
     return (
         <div className='footer-container'>
             <details>
-                <summary>Accordion One</summary>
-                Body Content 1
+                <summary>How does React work?</summary>
+                ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code
             </details>
             <details>
-                <summary>Accordion Two</summary>
-                Body Content 2
+                <summary>What is difference between state and props in React?</summary>
+                Simply put, State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props.
             </details>
             <details>
-                <summary>Accordion Two</summary>
-                Body Content 2
+                <summary>What are some other use cases for the useEffect () hook?</summary>
+                useEffect use cases
+                    Running once on mount: fetch API data.
+                    Running on state change: validating input field.
+                    Running on state change: live filtering.
+                    Running on state change: trigger animation on new array value.
+                    Running on props change: update paragraph list on fetched API data update.
             </details>
         </div>
     );
